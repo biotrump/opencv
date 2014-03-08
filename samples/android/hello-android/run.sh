6ac98fa75152914bc5c5463cae68bc6d083b9815
thomas tsai <thomas@life100.cc>
2014-03-09 00:05:52 +0800

    app is stored to /data/local/tmp for permission

--- samples/android/hello-android/run.sh
+++ samples/android/hello-android/run.sh
@@ -3,13 +3,13 @@ cd `dirname $0`
 PROJECT_NAME=hello-android
 
 # copy file to device (usually takes 10 seconds or more)
-adb push ./bin/$PROJECT_NAME /data/bin/sample/$PROJECT_NAME || return
+adb push ./bin/$PROJECT_NAME /data/local/tmp/$PROJECT_NAME || return
 
 # set execute permission
-adb shell chmod 777 /data/bin/sample/$PROJECT_NAME || return
+adb shell chmod 777 /data/local/tmp/$PROJECT_NAME || return
 
 # execute our application
-adb shell /data/bin/sample/$PROJECT_NAME || return
+adb shell /data/local/tmp/$PROJECT_NAME || return
 
 # get image result from device
 adb pull /mnt/sdcard/HelloAndroid.png || return
