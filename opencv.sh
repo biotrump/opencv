2834c039e4403a9ec3c1a68c6b041caac6945a1a
thomas tsai <thomas@biotrump.com>
2015-04-24 16:03:26 +0800

    local build to OPENCV_OUT=${OPENCV_SRC}/build/2.4.x-static

--- opencv.sh
+++ opencv.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 #. setup.sh
+#export CMAKE_BUILD_TYPE "Debug"
+export CMAKE_BUILD_TYPE="Release"
+set OPENCV_BUILD_SHARED_LIBS=0
+
 echo opencv.sh
+
 #make openCV out folder
-if [ ! -d ${BIOTRUMP_OUT} ]; then
-  echo "${BIOTRUMP_OUT} does not exist. mkdir"
-  mkdir ${BIOTRUMP_OUT}
-fi
-if [ ! -d ${BIOTRUMP_OUT}/openCV ]; then
-  echo "${BIOTRUMP_OUT}/openCV does not exist. mkdir"
-  mkdir ${BIOTRUMP_OUT}/openCV
-fi
-if [ ! -d ${OPENCV_OUT} ]; then
+#if [ ! -d ${BIOTRUMP_OUT} ]; then
+#  echo "${BIOTRUMP_OUT} does not exist. mkdir"
+#  mkdir ${BIOTRUMP_OUT}
+#fi
+#if [ ! -d ${BIOTRUMP_OUT}/openCV ]; then
+#  echo "${BIOTRUMP_OUT}/openCV does not exist. mkdir"
+#  mkdir ${BIOTRUMP_OUT}/openCV
+#fi
+export OPENCV_SRC=`pwd`
+export OPENCV_OUT=${OPENCV_SRC}/build/2.4.x-static
+
+if [ -d ${OPENCV_OUT} ]; then
+	rm -rf ${OPENCV_OUT}/*
+else
   echo "${OPENCV_OUT} does not exis. mkdir"
   mkdir ${OPENCV_OUT}
 fi
@@ -30,7 +40,7 @@ cmake -DBUILD_EXAMPLES:BOOL="1" -DBUILD_OPENEXR:BOOL="1" -DBUILD_SHARED_LIBS:BOO
 -DBUILD_opencv_world:BOOL="1" -DBUILD_JASPER:BOOL="1" -DBUILD_ZLIB:BOOL="1" \
 -DWITH_OPENCL:BOOL="0" -DWITH_OPENMP:BOOL="1" -DBUILD_TIFF:BOOL="1" -DBUILD_JPEG:BOOL="1" \
 -DWITH_OPENGL:BOOL="1" -DBUILD_PNG:BOOL="1" \
--DWITH_OPENCL:BOOL="1" \
+-DWITH_OPENCL:BOOL="1" -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
 ${OPENCV_SRC}
 time make $@
 popd
